520deec3e35078ae6b191f1e966c9f14857d93fe
Ahmed mohamed <mahmedrashid128@gmail.com>
2021-05-05 11:21:01 +0300

    Created a simple contact view template

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
-	"text/template"
 
 	"github.com/gorilla/mux"
 )
 
-var homeTemplate *template.Template
+var (
+	homeTemplate    *template.Template
+	contactTemplate *template.Template
+)
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
@@ -19,12 +21,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprintf(w, "To get in touch, please send an email to <a href=\"mailto:support@afrodott.com\">support@afrodott.com</a>.")
+	if err := contactTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 func main() {
 	var err error
 	homeTemplate, err = template.ParseFiles("views/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	contactTemplate, err = template.ParseFiles("views/contact.gohtml")
 	if err != nil {
 		panic(err)
 	}
