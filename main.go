ad52fd1377ad36fe93fa748adc034305c7cd1587
Ahmed Mohamed <44018986+AhmedARmohamed@users.noreply.github.com>
2021-05-27 19:42:00 +0300

    Created a controller for static pages and updated the View type to implement the http.Handler interface

--- main.go
+++ main.go
@@ -4,35 +4,19 @@ import (
 	"net/http"
 
 	"github.com/AhmedARmohamed/web-applications/controllers"
-	"github.com/AhmedARmohamed/web-applications/views"
 	"github.com/gorilla/mux"
 )
 
-var (
-	homeView    *views.View
-	contactView *views.View
-)
-
 func main() {
-	homeView = views.NewView("bootstrap", "views/home.gohtml")
-	contactView = views.NewView("bootstrap", "views/contact.gohtml")
+	staticC := controllers.NewStatic()
 	usersC := controllers.NewUsers()
 
 	r := mux.NewRouter()
-	r.HandleFunc("/", home).Methods("GET")
-	r.HandleFunc("/contact", contact).Methods("GET")
+	r.Handle("/", staticC.Home).Methods("GET")
+	r.Handle("/contact", staticC.Contact).Methods("GET")
 	r.HandleFunc("/signup", usersC.New).Methods("GET")
 	r.HandleFunc("/signup", usersC.Create).Methods("POST")
 
 	http.ListenAndServe(":3000", r)
 }
-
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	homeView.Render(w, nil)
-}
-
-func contact(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	contactView.Render(w, nil)
-}
+ 
