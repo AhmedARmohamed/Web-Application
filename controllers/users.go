d693201ac6fb89377b955556ca51c192537bbee3
Ahmed Mohamed <44018986+AhmedARmohamed@users.noreply.github.com>
2021-05-27 17:42:37 +0300

    Created our users controller and updated main.go to use it instead of signup function

---
+++ controllers/users.go
@@ -0,0 +1,38 @@
+package controllers
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/AhmedARmohamed/web-applications/views"
+)
+
+// NewUsers is used to create a new Users controller.
+// This function will panic if the templates are not
+// parsed correctly, and should only be used during
+// the initial setup.
+func NewUsers() *Users {
+	return &Users{
+		NewView: views.NewView("bootstrap", "views/users/new.gohtml"),
+	}
+}
+
+type Users struct {
+	NewView *views.View
+}
+
+// This is used to render the form where a user can create
+// a new account.
+// Get /signup
+func (u *Users) New(w http.ResponseWriter, r *http.Request) {
+	if err := u.NewView.Render(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+// This is to process the signup form when a user tries to
+// create a new user account.
+// Post /signup
+func (U *Users) Create(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "This is a fake message. Pretend that we created the user account")
+}
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/AhmedARmohamed/web-applications/controllers"
 	"github.com/AhmedARmohamed/web-applications/views"
 	"github.com/gorilla/mux"
 )
@@ -10,38 +11,28 @@ import (
 var (
 	homeView    *views.View
 	contactView *views.View
-	signupView  *views.View
 )
-
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(homeView.Render(w, nil))
-}
-
-func contact(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(contactView.Render(w, nil))
-}
-
-func signup(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(signupView.Render(w, nil))
-}
 
 func main() {
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
-	signupView = views.NewView("bootstrap", "views/signup.gohtml")
+	usersC := controllers.NewUsers()
 
 	r := mux.NewRouter()
-	r.HandleFunc("/", home)
-	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/signup", signup)
+	r.HandleFunc("/", home).Methods("GET")
+	r.HandleFunc("/contact", contact).Methods("GET")
+	r.HandleFunc("/signup", usersC.New).Methods("GET")
+	r.HandleFunc("/signup", usersC.Create).Methods("POST")
+
 	http.ListenAndServe(":3000", r)
 }
 
-func must(err error) {
-	if err != nil {
-		panic(err)
-	}
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	homeView.Render(w, nil)
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	contactView.Render(w, nil)
 }
